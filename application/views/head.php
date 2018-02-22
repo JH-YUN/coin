9fbbd2a54bf9f4271bd8e1b0a4dfe60e4cd078ff
JH-YUN <wnsgks789@gmail.com>
2018-02-22 16:24:16 +0900

    fixed link

--- application/views/head.php
+++ application/views/head.php
@@ -4,7 +4,8 @@
      <meta charset="utf-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link href="/codeigniter/static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <link href="../static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
    </head>
    <body>
      <nav class="navbar navbar-default">
@@ -33,8 +34,8 @@
           </form>
           <ul class="nav navbar-nav navbar-right">
 
-             <li><a href="/codeigniter/index.php/auth/login">Login</a></li>
-             <li><a href="/codeigniter/index.php/auth/register">회원가입</a></li>
+             <li><a href="#">Login</a></li>
+             <li><a href="#">회원가입</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">마이페이지 <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
