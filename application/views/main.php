56b0707838afce6174490f85a9613175a9b8b94d
JH-YUN <wnsgks789@gmail.com>
2018-02-28 22:17:31 +0900

    add several altcoins

--- application/views/main.php
+++ application/views/main.php
@@ -1,45 +1,7 @@
-<script type="text/javascript">
-//5초마다 BTC와 환율값을 받아오는 Ajax
-$(function(){
-  var btc_krw='loading';
-  var btc_usdt='loading';
-  var usd='loading';
-  var usdt='loading';
-  timer=setInterval(function(){
-    $.ajax({
-      type: 'post',
-      dataType: 'json',
-      url: 'http://ec2-52-79-213-156.ap-northeast-2.compute.amazonaws.com/coin/index.php/updatecoin/get_json',
-      success: function (data) {
-        btc_krw = data.krw;
-        btc_usdt= data.usdt;
-        // $('#BTC_bithumb').html(data.krw+" KRW");
-        // $('#BTC_binance').html(data.usdt+" USDT");
-        //alert('현재 BTC 가격은'+BTC+'원이다.');
-      },
-      error: function () {
-          alert('coin error');
-      }
-    });
-    $.ajax({
-      type: 'post',
-      dataType: 'json',
-      url: 'http://ec2-52-79-213-156.ap-northeast-2.compute.amazonaws.com/coin/index.php/updaterate/get_json',
-      success: function (data) {
-        usd= data.krw;
-        usdt=data.usdt;
-        //alert('현재 USD 가격은'+USD+'원이다.');
-      },
-      error: function () {
-          alert('rate error');
-      }
-    });
-    $('#BTC_bithumb').html(btc_krw+" KRW");
-    $('#BTC_binance').html(btc_usdt*usdt*usd+" KRW");
-    $('#premium').html(((btc_krw)-(btc_usdt*usdt*usd))*100/(btc_usdt*usdt*usd)+" %");
-  },5000);
-});
-</script>
+<div class="well well-sm" id="rate">
+  현재 1 USDT의 가격은 loading.. 입니다.<br>
+  현재 1 USD의 가격은 loading.. 입니다.
+</div>
 <table class="table">
   <tr>
     <th>코인 종류</th>
@@ -48,9 +10,27 @@ $(function(){
     <th>시세 차이</th>
   </tr>
   <tr>
-      <td>BTC</td>
-      <td id="BTC_bithumb">loading</td>
-      <td id="BTC_binance">loading</td>
-      <td id="premium">loading</td>
-    </tr>
+    <td>BTC</td>
+    <td id="BTC_bithumb">loading..</td>
+    <td id="BTC_binance">loading..</td>
+    <td id="BTC_premium">loading..</td>
+  </tr>
+  <tr>
+    <td>ETH</td>
+    <td id="ETH_bithumb">loading..</td>
+    <td id="ETH_binance">loading..</td>
+    <td id="ETH_premium">loading..</td>
+  </tr>
+  <tr>
+    <td>LTC</td>
+    <td id="LTC_bithumb">loading..</td>
+    <td id="LTC_binance">loading..</td>
+    <td id="LTC_premium">loading..</td>
+  </tr>
+  <tr>
+    <td>BCC</td>
+    <td id="BCC_bithumb">loading..</td>
+    <td id="BCC_binance">loading..</td>
+    <td id="BCC_premium">loading..</td>
+  </tr>
 </table>
