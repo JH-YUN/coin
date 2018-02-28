bf351ece174804141d60fa5be1de263d0e5a7b42
JH-YUN <wnsgks789@gmail.com>
2018-02-28 22:19:20 +0900

    change url

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,15 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends MY_Controller {
 	public function index()
 	{
-		$this->load->model('updaterate_model');
-		$this->load->model('updatecoin_model');
-		$BTCprice=$this->updatecoin_model->get('BTC');	//->krw KRW가격, ->usdt USDT가격
-		$USDrate=$this->updaterate_model->get();	//usd 환율 ->krw KRW환율 , ->usdt USDT환율
 		$this->_head();
-		$this->load->view('main',array(
-		'BTCprice'=>$BTCprice,
-		'USDrate'=>$USDrate
-		));
+		$this->load->view('main');
     $this->_footer();
 	}
 	public function board()
