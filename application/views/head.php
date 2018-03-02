3075faf8c750cb8231b19470a414453e72520dfb
JH-YUN <wnsgks789@gmail.com>
2018-03-02 19:57:25 +0900

    change bootstrap link href

--- application/views/head.php
+++ application/views/head.php
@@ -4,7 +4,7 @@
      <meta charset="utf-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link href="../static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <link href="/coin/static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
    </head>
    <body>
