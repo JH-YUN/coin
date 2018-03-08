ef41785c5eb1e09234d7e4d9a54236de4c26647d
JH-YUN <wnsgks789@gmail.com>
2018-03-09 02:49:22 +0900

    update

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -8,25 +8,6 @@ class Main extends MY_Controller {
 		$this->load->view('main');
     $this->_footer();
 	}
-	public function board()
-	{
-		$this->_head();
-		$this->load->view('board');
-		$this->_footer();
-	}
-	public function board_notice()
-	{
-		$this->_head();
-		$this->load->view('board_notice');
-		$this->_footer();
-	}
-	public function write()
-	{
-		$this->load->library('form_validation');
-		$this->_head();
-		$this->load->view('write');
-		$this->_footer();
-	}
 	public function login()
 	{
 		$this->_head();
