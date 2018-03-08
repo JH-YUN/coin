965f867cbccf670500eca6eae8d5a740ff6779e1
JH-YUN <wnsgks789@gmail.com>
2018-03-09 02:50:38 +0900

    pagination using CI lib

---
+++ application/config/pagination.php
@@ -0,0 +1,30 @@
+<?php
+$config['base_url']='http://localhost/coin/dev.php/board/index/';
+$config['first_url']='http://localhost/coin/dev.php/board/index/1';
+$config['full_tag_open']='<nav><ul class="pagination">';
+$config['full_tag_close']='</ul></nav>';
+$config['first_link']='&lt;&lt;';
+$config['last_link']='&gt;&gt;';
+$config['num_tag_open']="<li>";
+$config['num_tag_close']='</li>';
+$config['cur_tag_open']='<li class="active"><span>';
+$config['cur_tag_close']='</span></li>';
+$config['first_tag_open']="<li>";
+$config['first_tag_close']='</li>';
+$config['last_tag_open']="<li>";
+$config['last_tag_close']='</li>';
+$config['prev_tag_open']="<li>";
+$config['prev_tag_close']='</li>';
+$config['next_tag_open']="<li>";
+$config['next_tag_close']='</li>';
+$config['num_links']=5;
+$config['use_page_numbers'] = true;
+$config['first_disable_tag_open']='<li class="disabled">';
+$config['first_disable_tag_close']='</li>';
+$config['last_disable_tag_open']='<li class="disabled">';
+$config['last_disable_tag_close']='</li>';
+$config['prev_disable_tag_open']='<li class="disabled">';
+$config['prev_disable_tag_close']='</li>';
+$config['next_disable_tag_open']='<li class="disabled">';
+$config['next_disable_tag_close']='</li>';
+ ?>
