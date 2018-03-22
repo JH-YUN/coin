86c4f1d59f26c6aeef8a79927e333f7827b0da83
JH-YUN <wnsgks789@gmail.com>
2018-03-22 23:43:45 +0900

    modify css

--- application/views/head.php
+++ application/views/head.php
@@ -10,13 +10,19 @@
    </head>
    <body>
      <nav id="head-nav" class="navbar navbar-default">
-      <div  class="container-fluid">
+      <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
           <a class="navbar-brand" href="<?=site_url('main')?>"><span>Home</span></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
             <li id="head-nav-notice"><a href='<?=site_url("board/index_notice/1")?>'><span>공지</span></a></li>
             <li id="head-nav-index"><a href="<?=site_url('board/index/1')?>"><span>게시판</span> </a></li>
